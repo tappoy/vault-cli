1dd52c78cb9d738b5fea4e27f4e41bbd512fa25b
tappoy <tappoy1234@gmail.com>
2024-05-04 11:41:27 +0900

    add function to set dummy password

--- mock.go
+++ mock.go
@@ -11,3 +11,13 @@ var dummyPassword = "dummyPassword"
 func newPasswordInput() pwinput.PasswordInput {
 	return pwinput.NewDummyPasswordInput(dummyPassword)
 }
+
+func setDummyPassword(dummy string) string {
+	dummyPassword = dummy
+	return dummyPassword
+}
+
+func setInterruptPassword() string {
+	dummyPassword = pwinput.Interrupt
+	return dummyPassword
+}
