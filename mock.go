dd3b445f676df2a3807512e33b2c672114f23a68
tappoy <tappoy1234@gmail.com>
2024-05-04 13:32:39 +0900

    add debug message

--- mock.go
+++ mock.go
@@ -3,12 +3,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/tappoy/pwinput"
 )
 
 var dummyPassword = "dummyPassword"
 
 func newPasswordInput() pwinput.PasswordInput {
+	fmt.Println("RUNNING MOCK: dummyPassword: ", dummyPassword)
 	return pwinput.NewDummyPasswordInput(dummyPassword)
 }
 
